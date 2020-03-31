3f1e81f6ed9b6eb16a55f036e20147d2956c1a1e
Frantz-B <Frantz@kargo.com>
2020-03-31 17:59:43 -0400

    Adding UI login test cases

---
+++ cypress/integration/ui/login-logout.js
@@ -0,0 +1,29 @@
+context('Login-Logout', () => {
+    describe('Login-Logout UI', () => {
+      it('Login using valid credentials', () => {
+        cy.visit('');
+        cy.get('[placeholder="Email"]').focus().clear().type('Frantz@kargo.com', );
+        cy.get('[placeholder="Password"]').focus().clear().type('admin', );
+        cy.get('[id="kt_login_signin_submit"]').click().then(() => {
+          cy.url().should('include', 'advertisers');
+        });
+      });
+
+      it('Logout', () => {
+        cy.get('[class="kt-header__topbar-icon"]').click(); // clicking avatar
+        cy.get('[class="btn btn-label-brand btn-sm btn-bold"]').first().click().then(() => {  // clicking on sign out
+          cy.url().should('include', 'login?returnUrl');// User redirected to login page
+        });
+      });
+
+      it('Login using invalid credentials', () => {
+        cy.visit('');
+        cy.get('[placeholder="Email"]').focus().clear().type('eh@kargo.com', );
+        cy.get('[placeholder="Password"]').focus().clear().type('wrong', );
+        cy.get('[id="kt_login_signin_submit"]').click();
+        cy.get('[class="alert-text"]').should('contain', 'The login detail is incorrect');
+      });
+
+      xit('Forget Password');
+    });
+  });
